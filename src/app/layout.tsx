94f92cb0201a5d78306d25d210048a92ba404f7f
Filip Pacurar <filip@pacurar.net>
2025-03-21 23:26:26 +0200

    Update page title for clarity and branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Ceac Checker - Try to catch me!',
+  title: 'Ceac chances checker',
   description: 'A fun and interactive case number checker with a twist',
 };
 
